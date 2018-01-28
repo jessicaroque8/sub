4142d851ada0559cb759eec71000dff93672b6c7
Jessica Roque <jroque.dev@gmail.com>
2018-01-27 18:36:42 -0800

    Push frontend directory.

---
+++ frontend/node_modules/angular2-token-ionic3/lib/a2t-ui/a2t-shared/a2t-headline/a2t-headline.component.js
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+var A2tHeadlineComponent = (function () {
+    function A2tHeadlineComponent() {
+    }
+    A2tHeadlineComponent.decorators = [
+        { type: Component, args: [{
+                    selector: 'a2t-headline',
+                    template: '<p><ng-content></ng-content></p>',
+                    styles: ["\n        p {\n            text-align: center;\n            color: white;\n            font-size: 30px;\n        }\n    "]
+                },] },
+    ];
+    /** @nocollapse */
+    A2tHeadlineComponent.ctorParameters = function () { return []; };
+    return A2tHeadlineComponent;
+}());
+export { A2tHeadlineComponent };
+//# sourceMappingURL=a2t-headline.component.js.map
