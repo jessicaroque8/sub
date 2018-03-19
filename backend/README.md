0b01ddbfad80ce156080f545202557459e00fda8
Jessica Roque <jroque.dev@gmail.com>
2018-03-19 13:49:57 -0700

    README and seed updates.

--- backend/README.md
+++ backend/README.md
@@ -1,2 +1,24 @@
 # sub
 A MINDBODY API client to manage staff sub requests.
+
+To launch:
+1. Install dependencies as needed:
+   * Ruby (https://www.ruby-lang.org/en/downloads/)
+   * Rails
+      `$ gem install rails`
+   * MINDBODY Developer Account (https://developers.mindbodyonline.com/Home/SignUp)
+2. Install the project gems.
+   `$ bundle install`
+3. Configure environment variables with your MINDBODY Sandbox API credentials using the Figaro gem.
+   * `$ bundle exec figaro install`
+   * In config/application.yml:
+   ```
+   mindbody_source_name: SOURCE_NAME
+   mindbody_source_key: SOURCE_KEY
+   mindbody_siteid: SITE_ID
+   ```
+4. Follow the instructions in db/seed.rb to modify the file with seeds relevant to your MINDBODY Sandbox. Then,
+   `$ rails db:reset`
+5. Launch the server.
+   `$ rails server`
+6. Setup and launch the client. See the README for sub_frontend submodule.
--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,10 +1,21 @@
 require 'random_data'
 
+# The following seed info reflects the data from my personal MINDBODY Sandbox.
+# You will need to create seed data for your own Sandbox:
+# 1. Identify staff names in the Class Schedule on the MINDBODY Sandbox
+# 2. Open the Rails console:
+#      $ rails c
+#      $ require 'MindBodyAPI'
+#      $ mb = MindBodyAPI.new
+#      $ mb.get_single_staff('any_username', 'any_password', staff1_first_name, staff1_last_name)
+#      $ mb.get_single_staff('any_username', 'any_password', staff2_first_name, staff2_last_name)
+# 3. Use the below template to create an account for each staff member.
 
-jennifer = User.create!(staff_id_mb: 100000164, first_name: 'Jennifer', last_name: 'Anderson', email: 'jennifer@email.com', password: 'saraswati8', image: 'https://image.ibb.co/cMrBeR/image_2.png')
-johnny = User.create!(staff_id_mb: 100000270, first_name: 'Johnny', last_name: 'Salke', email: 'johnny@email.com', password: 'prajaprati', image: 'https://thumb.ibb.co/nGmcKR/image_3.png')
-
-users = User.all
+# Template:
+# jennifer = User.create!(staff_id_mb: 100000164, first_name: 'Jennifer', last_name: 'Anderson', email: 'jennifer@email.com', password: 'saraswati8', image: 'https://image.ibb.co/cMrBeR/image_2.png')
+# johnny = User.create!(staff_id_mb: 100000270, first_name: 'Johnny', last_name: 'Salke', email: 'johnny@email.com', password: 'prajaprati', image: 'https://thumb.ibb.co/nGmcKR/image_3.png')
+#
+# users = User.all
 
 yoga = Group.create!(name: 'Yoga')
 fitness = Group.create!(name: 'Fitness')
@@ -16,24 +27,7 @@ users.each do |user|
    end
 end
 
-# Open
-# a = SubRequest.create!(user: johnny, group: yoga, class_id_mb: 5555, start_date_time: DateTime.new(2018, 04, 01, 8, 00, 00), end_date_time: DateTime.new(2018, 04, 01, 9, 00, 00), class_name: RandomData.word, note: RandomData.sentence)
-# b = SubRequest.create!(user: jennifer, group: fitness, class_id_mb: 00000, start_date_time: DateTime.new(2018, 04, 9, 10, 00, 00), end_date_time: DateTime.new(2018, 04, 9, 11, 00, 00), class_name: RandomData.word, note: RandomData.sentence)
-# c = SubRequest.create!(user: jennifer, group: yoga, class_id_mb: 77777, start_date_time: DateTime.new(2018, 04, 10, 11, 00, 00), end_date_time: DateTime.new(2018, 04, 10, 12, 00, 00), class_name: RandomData.word, note: RandomData.sentence)
-#
-# sub_requests = SubRequest.all
-
-# Open
-# a_sendee = Sendee.create!(user: jennifer, sub_request: a)
-# b_sendee = Sendee.create!(user: johnny, sub_request: b)
-#
-# sendees = Sendee.all
-
-
-
 
 puts "Seeds finished"
 puts "#{User.count} users created"
 puts "#{Group.count} groups created"
-puts "#{SubRequest.count} sub requests created"
-puts "#{Sendee.count} sendees created"
